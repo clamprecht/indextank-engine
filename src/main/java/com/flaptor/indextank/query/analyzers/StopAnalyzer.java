e4ac1313d68dab07ebe4d9eae4f61a80d7a74dfe
Chris Lamprecht <clamprecht@gmail.com>
2012-06-10 18:36:23 -0500

    StopAnalyzer extends StopwordAnalyzerBase now so it will compile, but should probably be removed soon

--- src/main/java/com/flaptor/indextank/query/analyzers/StopAnalyzer.java
+++ src/main/java/com/flaptor/indextank/query/analyzers/StopAnalyzer.java
@@ -20,18 +20,21 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.StopwordAnalyzerBase;
 import org.apache.lucene.util.Version;
 import org.json.simple.JSONArray;
 
 import com.google.common.collect.ImmutableSet;
 
-public abstract class StopAnalyzer extends StandardAnalyzer {
+/**
+ * @deprecated remove this
+ */
+public abstract class StopAnalyzer extends StopwordAnalyzerBase {
     
     private static final String STOPWORDS = "stopwords";
     
     public StopAnalyzer(Map<Object, Object> analyzerConfiguration) {
-        super(Version.LUCENE_CURRENT, getStopWords(analyzerConfiguration));
+        super(Version.LUCENE_36, getStopWords(analyzerConfiguration));
     }
 
     private static Set<String> getStopWords(Map<Object, Object> analyzerConfiguration) {
