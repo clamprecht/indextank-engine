4ee41d6644c722fde82834318e01b8c2770fd85b
Chris Lamprecht <clamprecht@gmail.com>
2012-06-10 18:45:13 -0500

    Adding testPayload()

--- src/test/java/com/flaptor/indextank/index/lsi/LsiIndexerTest.java
+++ src/test/java/com/flaptor/indextank/index/lsi/LsiIndexerTest.java
@@ -21,8 +21,15 @@ import static com.flaptor.util.TestInfo.TestType.UNIT;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.PayloadAttribute;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Payload;
+import org.apache.lucene.util.ReaderUtil;
 
 import com.flaptor.indextank.index.Document;
 import com.flaptor.indextank.index.scorer.MockScorer;
@@ -67,8 +74,9 @@ public class LsiIndexerTest extends TestCase {
         lsiIndexer.add("A",doc);
         lsiIndexer.makeDirectoryCheckpoint();
 
-        assertEquals("Wrong document count", 1, index.getLuceneIndexWriter().numDocs());
-        assertTrue("fieldC not found", index.getLuceneIndexWriter().getReader().getFieldNames(IndexReader.FieldOption.INDEXED).contains("fieldC"));
+        IndexWriter writer = index.getLuceneIndexWriter();
+        assertEquals("Wrong document count", 1, writer.numDocs());
+        assertTrue("fieldC not found", ReaderUtil.getMergedFieldInfos(IndexReader.open(writer, false)).fieldInfo("fieldC") != null);
 	}
 	
     @TestInfo(testType=UNIT)
@@ -128,5 +136,22 @@ public class LsiIndexerTest extends TestCase {
         assertEquals("Wrong document count", 2, index.getLuceneIndexWriter().numDocs());
 	}
 
-	
+    @TestInfo(testType=UNIT)
+    public void testPayload() throws Exception {
+        PayloadAnalyzer analyzer = new PayloadAnalyzer();
+        TokenStream ts = analyzer.tokenStream(LsiIndex.PAYLOAD_TERM_FIELD, new StringReader("User 1234"));
+        assertTrue(ts.incrementToken());
+        TermAttribute ta = ts.getAttribute(TermAttribute.class);
+        assertEquals(LsiIndex.PAYLOAD_TERM_FIELD, ta.term());
+        // todo:
+        //CharTermAttribute ta = ts.getAttribute(CharTermAttribute.class);
+        //System.out.println("Term: " + ta.toString());
+        PayloadAttribute pa = ts.getAttribute(PayloadAttribute.class);
+        Payload payload = pa.getPayload();
+        String payloadString = new String(payload.getData(), "UTF-8");
+        assertEquals("User 1234", payloadString);
+//        System.out.println("Payload: " + payloadString);
+        assertFalse(ts.incrementToken());
+    }
+
 }
